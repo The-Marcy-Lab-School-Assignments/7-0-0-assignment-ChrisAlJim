d599645f470f096d9fac183f0b1f74a491f1b920
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-08-12 22:22:40 -0400

    feedback

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+/** FEEDBACK: You are really close, you have your components however, you are missing your state to make your greeting dynamic! */
 const SizeButtons = () => {
   return (
     <>
